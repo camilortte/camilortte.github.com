9fc84b382761c73af9f0fd7ebc3af6e01f567442
Camilo Ramirez <camilortte@hotmail.com>
2019-05-01 12:43:00 -0500

    Added compoentn About

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class About extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="About - Camilo Ramirez" />
+        <h1>About</h1>
+        <p>Hola</p>
+      </Layout>
+    )
+  }
+}
+
+
+export default About
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
