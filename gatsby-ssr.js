460815a22c109205bc6241480876e009f8934e38
Camilo Ramirez <camilortte@hotmail.com>
2019-05-16 23:37:55 -0500

    Updated preconnect

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,12 +3,12 @@ import React from "react";
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <link
-      rel="dns-prefetch"
+      rel="preconnect"
       key="dns-prefetch-marketingplatform-google"
       href="https://marketingplatform.google.com"
     />,
     <link
-      rel="dns-prefetch"
+      rel="preconnect"
       key="dns-prefetch-google"
       href="https://www.google.com"
     />
